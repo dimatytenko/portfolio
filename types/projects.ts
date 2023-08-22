3d0d19946f1af3026dc40dbd50a385118e62064d
Dima Tytenko <amgpetronass@gmail.com>
2023-08-22 14:48:19 +0300

    fixed build

--- types/projects.ts
+++ types/projects.ts
@@ -1,16 +1,16 @@
 import {type} from 'os';
 
 export type TProject = {
+  imgSrc: string;
   title: string;
   description: string;
   technology: string;
   technologies: string[];
   linkGithub: string;
   linkDemo: string;
-  team: boolean;
-  countTeam: number;
-  role: string;
-  imgSrc: string;
+  team?: boolean;
+  countTeam?: number;
+  role?: string;
 };
 
 export type TProjects = TProject[];
