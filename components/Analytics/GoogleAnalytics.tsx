82ea2c47bdbd18e46d6807d1f6e80a7819cb20e2
Dima Tytenko <amgpetronass@gmail.com>
2023-08-26 19:02:44 +0300

    update

--- components/Analytics/GoogleAnalytics.tsx
+++ components/Analytics/GoogleAnalytics.tsx
@@ -2,11 +2,24 @@
 
 import Script from 'next/script';
 
+import {usePathname, useSearchParams} from 'next/navigation';
+import {useEffect} from 'react';
+import {pageview} from '@/lib/gtagHelper';
+
 export default function GoogleAnalytics({
   GA_MEASUREMENT_ID,
 }: {
   GA_MEASUREMENT_ID: string;
 }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+
+    pageview(GA_MEASUREMENT_ID, url);
+  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
+
   return (
     <>
       <Script
