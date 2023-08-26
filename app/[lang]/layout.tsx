08fd70b6465fcd52df93b4ce89204690ab3af7b7
Dima Tytenko <amgpetronass@gmail.com>
2023-08-26 21:21:24 +0300

    remove changes

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -29,8 +29,6 @@ export default async function RootLayout({
 }) {
   const dictionary = await getDictionary(lang);
 
-  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-
   return (
     <html lang={lang}>
       {/* connection Search Console */}
@@ -56,5 +54,5 @@ export const metadata: Metadata = {
     apple: ['/apple-touch-icon.png?v=4'],
     // shortcut: ['/apple-touch-icon.png'],
   },
-  manifest: '/manifest',
+  // manifest: '/manifest',
 };
--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,5 +5,3 @@ const nextConfig = {
     styledComponents: true,
   },
 };
-
-module.exports = withPWA(nextConfig);
